cb026a7d644b7bf6fc475c44789e218123276859
MiguelMouraa <miguel.mourasp@gmail.com>
2026-04-01 11:04:22 -0300

    Alteração do Logo

--- src/app/Header.js
+++ src/app/Header.js
@@ -1,13 +1,14 @@
-// components/Header.js
-"use client";
-
-import React from 'react';
-
 export default function Header() {
   return (
     <header className="bg-gray-800 text-white py-4">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="maintext">Bolos da Fabi</h1>
+        
+        <img
+          src="/logo.png"
+          alt="Bolos e Tortas da Fabi"
+          className="h-16"
+        />
+
         <div className="header-links flex items-center space-x-6">
           <a
             href="https://wa.me/5511985053701"
@@ -18,10 +19,11 @@ export default function Header() {
             <img
               src="/logowpp.png"
               alt="WhatsApp Logo"
-              className="logowpp w-6 h-6 mr-2"
+              className="w-6 h-6 mr-2"
             />
             (11) 98505-3701
           </a>
+
           <a
             href="https://www.instagram.com/bolossdafabi?igsh=Y2dyYnM3emxidzAy"
             target="_blank"
@@ -31,11 +33,12 @@ export default function Header() {
             <img
               src="/logoinsta.png"
               alt="Instagram Logo"
-              className="logoinsta w-6 h-6 mr-2"
+              className="w-6 h-6 mr-2"
             />
             Instagram
           </a>
         </div>
+
       </div>
     </header>
   );
